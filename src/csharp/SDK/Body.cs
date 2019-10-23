0766af676bb2f6e10ab1f519a509aa68ad9da8c8
Cameron Vetter <cameron@cameronvetter.com>
2019-10-23 15:22:33 -0500

    Added Body Tracking SDK support

---
+++ src/csharp/SDK/Body.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Microsoft.Azure.Kinect.BodyTracking;
+
+namespace Microsoft.Azure.Kinect.Sensor
+{
+    [StructLayout(LayoutKind.Sequential)]
+    [Sensor.Native.NativeReference("k4abt_body_t")]
+    public struct Body
+    {
+        [CLSCompliant(false)]
+        public uint Id;
+
+        public Skeleton Skeleton;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    [Sensor.Native.NativeReference("k4abt_joint_t")]
+    public struct Joint
+    {
+        public Vector3 Position;
+
+        public Quaternion Orientation;
+
+        public JointConfidenceLevel ConfidenceLevel;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    [Sensor.Native.NativeReference("k4abt_skeleton_t")]
+    public struct Skeleton
+    {
+        [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.Struct, SizeConst = (int)BodyFrame.JointId.Count)]
+        public Joint[] Joints;
+    }
+}
---
+++ src/csharp/SDK/SensorOrientation.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Kinect.Sensor.Native;
+
+namespace Microsoft.Azure.Kinect.Sensor
+{
+    [NativeReference]
+    public enum SensorOrientation
+    {
+        DEFAULT = 0,
+        CLOCKWISE90,
+        COUNTERCLOCKWISE90,
+        FLIP180,
+    }
+}
